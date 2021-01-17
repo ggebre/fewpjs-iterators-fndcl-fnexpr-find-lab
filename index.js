a9b66bdb1daf1010d22ee06c1f4a7d715594cbd1
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-17 14:39:04 -0600

    commit tree 13949d893c34bdc455af702ef5b808e6f8ee80f3

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  const foundElement = array.find(element => element.result === "W")
+  return foundElement ? foundElement.year : foundElement
+}
